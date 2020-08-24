8678aa9870de07591c6dc288a7e2e37fffb2d4ff
Chip Thien <themacrochip@gmail.com>
2020-08-24 12:04:18 -0500

    Persist and restore data

--- background.js
+++ background.js
@@ -2,61 +2,105 @@
 
 const DEBUG = true;
 
-let id = 0;
-let tabs = {};
-let tabConnections = [];
-chrome.storage.local.get(['id', 'tabs', 'tabConnections'], function(result) {
-  console.log(`data initialized as ${JSON.stringify(result, null, 2)}`);
-  if (result.id) {
-    id = result.id;
-  }
-  if (result.tabs) {
-    tabs = result.tabs;
-  }
-  if (result.tabConnections) {
-    tabConnections = result.tabConnections;
-  }
-});
+class Datastore {
 
-const redraw = () => {
+  constructor() {
+    this.id = { id: 0 };
+    this.tabs = {};
+    this.tabConnections = [];
+    this.loaded = false;
+  }
+
+  data() {
+    return new Promise(res => {
+      if (!this.loaded) {
+        chrome.storage.local.get(['id', 'tabs', 'tabConnections'], (result) => {
+          console.log(`data initialized as ${JSON.stringify(result, null, 2)}`);
+          if (result.id) {
+            this.id = result.id;
+          }
+          if (result.tabs) {
+            this.tabs = result.tabs;
+          }
+          if (result.tabConnections) {
+            this.tabConnections = result.tabConnections;
+          }
+          this.loaded = true;
+          res({
+            id: this.id,
+            tabs: this.tabs,
+            tabConnections: this.tabConnections,
+          });
+        });
+      } else {
+        res({
+          id: this.id,
+          tabs: this.tabs,
+          tabConnections: this.tabConnections,
+        });
+      }
+    });
+  };
+
+  save() {
+    const newData = {
+      id: this.id,
+      tabs: this.tabs,
+      tabConnections: this.tabConnections,
+    };
+    chrome.storage.local.set(newData, () => {
+      console.log(`Saved data as ${JSON.stringify(newData, null, 2)}`);
+    });
+  }
+
+  reset() {
+    this.id = { id: 0 };
+    this.tabs = {};
+    this.tabConnections = [];
+    this.save();
+  }
+};
+
+const datastore = new Datastore();
+
+const redraw = async () => {
+  datastore.save();
+  let data = await datastore.data();
   chrome.extension.sendMessage({
     type: 'RES_GET_GRAPH',
     data: {
-      tabs,
-      tabConnections,
+      tabs: data.tabs,
+      tabConnections: data.tabConnections,
     },
   });
 }
 
-const makeNode = (label) => {
-  return new Promise(res => {
-    const newNode = { id, label };
-    id += 1;
-    chrome.storage.local.set({'id': id}, function() {
-      console.log(`id saved as ${id}`);
-      res(newNode);
-    });
-  });
+const makeNode = async (label) => {
+  let data = await datastore.data();
+  const newNode = { "id": data.id.id, label };
+  data.id.id += 1;
+  return newNode;
 };
 
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   const tabId = sender.tab.id;
   console.log(`${tabId} message ${request.type}`);
   if (request.type === "NEW_PAGE") {
-    if (!tabs[tabId]) { //if the addon wasn't up when the new tab listener fired
+    let data = await datastore.data();
+    if (!data.tabs[tabId]) { //if the addon wasn't up when the new tab listener fired
       console.log(`new tab from content script at ${Date.now()}`);
-      tabs[tabId] = {
+      data.tabs[tabId] = {
         nodes: [await makeNode(request.href)],
         edges: [],
       };
     } else {
       console.log(`${tabId} clicked ${request.href} at ${Date.now()}`);
-      const nodes = tabs[tabId].nodes;
-      const edges = tabs[tabId].edges;
+      const nodes = data.tabs[tabId].nodes;
+      const edges = data.tabs[tabId].edges;
       nodes.push(await makeNode(request.href));
       const lastNode = nodes[nodes.length - 2];
       if (lastNode) {
-        edges.push({ from: lastNode.id, to: id - 1 });
+        edges.push({ from: lastNode.id, to: data.id.id - 1 });
       }
     }
     redraw();
@@ -65,9 +109,7 @@ chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
     redraw();
   }
   if (request.type === "RESET") {
-    id = 0;
-    tabs = {};
-    tabConnections = [];
+    datastore.reset();
     redraw();
   }
 });
@@ -76,22 +118,23 @@ chrome.tabs.onCreated.addListener(async (tab) => {
   const openerTabId = tab.openerTabId;
   const tabId = tab.id;
   console.log(`Tab ${tabId} opened by ${openerTabId} at ${Date.now()}`);
-  if (!tabs[tabId]) {
+  let data = await datastore.data();
+  if (!data.tabs[tabId]) {
     console.log(`New tab node`);
-    tabs[tabId] = {
+    data.tabs[tabId] = {
       nodes: [await makeNode(`New tab`)],
       edges: [],
     };
   }
   if (openerTabId && tabId) {
-    if (!tabs[openerTabId]) {
+    if (!data.tabs[openerTabId]) {
       console.log(`No data for ${openerTabId}. Perhaps the tab was already open before the extension was loaded`);
       return;
     }
     console.log(`New tab edge`);
-    const openerNodes = tabs[openerTabId].nodes;
-    const newTabNodes = tabs[tabId].nodes;
-    tabConnections.push({
+    const openerNodes = data.tabs[openerTabId].nodes;
+    const newTabNodes = data.tabs[tabId].nodes;
+    data.tabConnections.push({
       from: openerNodes[openerNodes.length - 1].id,
       to: newTabNodes[newTabNodes.length - 1].id,
     });
