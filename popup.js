a02374d421a0702773dddd068d925f249abb64c6
Chip Thien <themacrochip@gmail.com>
2020-08-25 22:02:13 -0500

    Cleanup

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 'use strict';
 
-document.getElementById('changeColor').onclick = function(element) {
+document.getElementById('changeColor').onclick = element => {
   chrome.tabs.create({url: chrome.extension.getURL('graph_ui.html')});
 };
