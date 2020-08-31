1507053de75c0bf8acbb1f30ba6ce6b996afa2dd
Chip Thien <themacrochip@gmail.com>
2020-08-31 00:51:38 -0500

    Add title change watcher to be a quick n dirty page change monitor

--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,11 @@
 chrome.runtime.sendMessage({ type: "NEW_PAGE", href: window.location.href, title: document.title });
+let target = document.querySelector('title');
+new window.MutationObserver(
+    mutations => {
+        mutations.forEach(
+            mutation => {
+                chrome.runtime.sendMessage({ type: "NEW_PAGE", href: window.location.href, title: mutation.target.textContent });
+            }
+        );
+    }
+).observe(target, { subtree: true, characterData: true, childList: true });
