0f950343c1bc356cb98c868b3519590aa3f176b5
Chip Thien <themacrochip@gmail.com>
2020-09-01 20:59:37 -0500

    Watch for items being added to history so that I have a 1 to 1 correlation with history items

--- background.js
+++ background.js
@@ -9,6 +9,7 @@ class Datastore {
     this.tabs = {};
     this.tabConnections = [];
     this.loaded = false;
+    this.tabUpdated = {};
   }
 
   _data() {
@@ -16,13 +17,14 @@ class Datastore {
       id: this.id,
       tabs: this.tabs,
       tabConnections: this.tabConnections,
+      tabUpdated: this.tabUpdated,
     };
   }
 
   data() {
     return new Promise(res => {
       if (!this.loaded) {
-        chrome.storage.local.get(['id', 'tabs', 'tabConnections'], (result) => {
+        chrome.storage.local.get(['id', 'tabs', 'tabConnections', 'tabUpdated'], (result) => {
           console.log(`data initialized as ${JSON.stringify(result, null, 2)}`);
           if (result.id) {
             this.id = result.id;
@@ -32,6 +34,9 @@ class Datastore {
           }
           if (result.tabConnections) {
             this.tabConnections = result.tabConnections;
+          }
+          if (result.tabUpdated) {
+            this.tabUpdated = result.tabUpdated;
           }
           this.loaded = true;
           res(this._data());
@@ -56,6 +61,7 @@ class Datastore {
     this.id = { id: 0 };
     this.tabs = {};
     this.tabConnections = [];
+    this.tabUpdated = {};
     await this.save();
   }
 };
@@ -90,26 +96,6 @@ chrome.commands.onCommand.addListener(command => {
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
   const tabId = sender.tab.id;
   console.log(`${tabId} message ${request.type}`);
-  if (request.type === "NEW_PAGE") {
-    let data = await datastore.data();
-    if (!data.tabs[tabId]) { //if the addon wasn't up when the new tab listener fired
-      console.log(`new tab from content script at ${Date.now()}`);
-      data.tabs[tabId] = {
-        nodes: [await makeNode(request.title, request.href)],
-        edges: [],
-      };
-    } else {
-      console.log(`${tabId} clicked ${request.href} title ${request.title} at ${Date.now()}`);
-      const nodes = data.tabs[tabId].nodes;
-      const edges = data.tabs[tabId].edges;
-      nodes.push(await makeNode(request.title, request.href));
-      const lastNode = nodes[nodes.length - 2];
-      if (lastNode) {
-        edges.push({ from: lastNode.id, to: data.id.id - 1 });
-      }
-    }
-    redraw();
-  }
   if (request.type === "REQ_GET_GRAPH") {
     redraw();
   }
@@ -143,6 +129,50 @@ chrome.tabs.onCreated.addListener(async (tab) => {
       from: openerNodes[openerNodes.length - 1].id,
       to: newTabNodes[newTabNodes.length - 1].id,
     });
+  }
+  redraw();
+});
+
+const addNodeToExistingTabTree = async (data, tabId, title, url) => {
+  const nodes = data.tabs[tabId].nodes;
+  const edges = data.tabs[tabId].edges;
+  nodes.push(await makeNode(title, url));
+  const lastNode = nodes[nodes.length - 2];
+  if (lastNode) {
+    edges.push({ from: lastNode.id, to: data.id.id - 1 });
+  }
+};
+
+chrome.history.onVisited.addListener(async historyItem => {
+  console.log(`History onVisited ${JSON.stringify(historyItem, null, 2)}`);
+  let data = await datastore.data();
+  const tabId = data.tabUpdated[historyItem.url]
+  if (!data.tabs[tabId]) {
+    console.log(`Making first node for tabId ${tabId} in history.onVisisted`);
+    data.tabs[tabId] = {
+      nodes: [await makeNode(`${historyItem.title}`, `${historyItem.url}`)],
+      edges: [],
+    };
+  } else {
+    console.log(`Existing node(s) found for tabId ${tabId} in history.onVisited`);
+    await addNodeToExistingTabTree(data, tabId, historyItem.title, historyItem.url);
+  }
+  redraw();
+});
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+  console.log(`${tabId} updated Url ${changeInfo.url} title ${changeInfo.title}`);
+  let data = await datastore.data();
+  if (changeInfo.url) {
+    data.tabUpdated[changeInfo.url] = tabId; //this does not work if multiple tabs visit the same url before the history listener fires to consume this assignment
+  }
+  if (changeInfo.title) {
+    //TODO restrict this to title updates after onVisited
+    console.log(`onupdated tabs at tab id ${JSON.stringify(data.tabs[tabId], null, 2)}`);
+    if (data.tabs[tabId]) {
+      const nodes = data.tabs[tabId].nodes;
+      nodes[nodes.length - 1].label = changeInfo.title;
+    }
   }
   redraw();
 });
